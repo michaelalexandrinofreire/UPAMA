78e9053568ad1f1280be9de28bc6709ef7cbd0cf
Michael <104912294+michaelalexandrinofreire@users.noreply.github.com>
2023-06-04 22:00:37 -0300

    ações e footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import NavBar from '@/components/NavBar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +24,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <NavBar/>
         {children}
-        </body>
+        <Footer/>
+      </body>
     </html>
   )
 }
