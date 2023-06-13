27344eb94024c8403d02eb8bb8343cc6509e8cda
Michael Alexandrino Freire <alexandrino.michael07@aluno.ifce.edu.br>
2023-06-13 20:22:09 -0300

    adicionando funções nas pages servicos e contatos

---
+++ src/app/contatos/page.tsx
@@ -0,0 +1,7 @@
+export default function Contatos(){
+    return(
+        <main>
+            
+        </main>
+    )
+}
---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,5 @@
+export default function Servicos(){
+    return(
+        <main></main>
+    )
+}
