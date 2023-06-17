1731fa29c942a1c4bd0b949c7d80a390978d55c5
Michael Alexandrino Freire <alexandrino.michael07@aluno.ifce.edu.br>
2023-06-17 19:54:14 -0300

    adicionando a função de mudar o bg dos botões e o fill dos svgs ao clicar nos mesmos, como também juntando as funções em uma

--- src/components/Acao.tsx
+++ src/components/Acao.tsx
@@ -1,19 +1,30 @@
-import Button from "./Button";
+"use client"
+import { useState } from 'react';
 import styles from '../style/acoes.module.css'
 
 interface AcaoProps {
   title: string;
   description: string;
   imageSrc: string;
+  otherDescription: String;
 }
 
-export default function Acao({ title, description, imageSrc }: AcaoProps) {
+export default function Acao({ title, description, imageSrc, otherDescription }: AcaoProps) {
+  const [showDescription, setShowDescription] = useState(false);
+  
+  const handleButtonClick = () => {
+    setShowDescription(!showDescription);
+  };
+
   return (
     <div className={styles.acao}>
       <img src={imageSrc} alt="" />
       <h3>{title}</h3>
-      <p>{description}</p>
-      <Button text="Leia mais" />
+      <div>
+        <p>{description}</p>
+        {showDescription && <p className={styles.otherDescription}>{otherDescription}</p>}
+      </div>
+      <button className={styles.button} onClick={handleButtonClick}>Leia Mais</button>
       
     </div>
   );
--- src/components/Acoes.tsx
+++ src/components/Acoes.tsx
@@ -10,17 +10,21 @@ export default function Acoes(){
     <Acao title="Educação Ambiental" 
     description="A educação ambiental objetiva a compreensão dos 
     conceitos relacionados ao meio ambiente, sustentabilidade, 
-    preservação e conservação."
+    preservação e conservação. Tendo o"
+    otherDescription="impacto de despertar uma consciência crítica em relação aos impactos
+     das ações humanas no meio ambiente."
     imageSrc="/images/educacaoambiental.jfif"/>
     <Acao title="Resgate de fauna"
     description="A equipe de resgate de animais silvestre da Instituição UPAMA,
      tem a enorme alegria e gratidão de poder contribuir na preservação da nossa fauna,
-      através do resgate de animais em situação de vulnerabilidade."
+      através do"
+      otherDescription="resgate de animais em situação de vulnerabilidade."
     imageSrc="/images/fauna.jfif"/>
     <Acao title="Reflorestamento"
     description="A instituição UPAMA contribuir para o reflorestamento de áreas degradadas
      e desmatadas, com o plano de manejo que monitora o desenvolvimento natural da área e 
-     com o plantio de mudas de forma adequada."
+     com o"
+     otherDescription="plantio de mudas de forma adequada."
      imageSrc="/images/reflorestamento.jfif"/>
    </div>
    <div className={styles.containerAgende}>
