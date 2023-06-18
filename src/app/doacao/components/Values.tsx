3b4f533c5bf3a0ada9375a0dd9a13a39c1b22a1a
Michael Alexandrino Freire <alexandrino.michael07@aluno.ifce.edu.br>
2023-06-17 21:11:46 -0300

    adicionando função que muda href do botao continuar ao clicar em algum dos botoes de valores, como também desabilita o botão continuar enquanto algum botao de valor não for clicado

--- src/app/doacao/components/Values.tsx
+++ src/app/doacao/components/Values.tsx
@@ -4,10 +4,29 @@ import style from '../style/globals.module.css';
 
 export default function Values() {
   const [selectedButton, setSelectedButton] = useState<number | null>(null);
+  const [linkHref, setLinkHref] = useState('');
+  const [botaoClicado, setBotaoClicado] = useState(false);
 
-  function handleButtonClick(index: number) {
-    setSelectedButton(index);
-  }
+    function setLinkUm () {
+      setLinkHref('https://sacola.pagseguro.uol.com.br/0ad3545a-8ad1-4423-9443-77d17253b792');
+      setBotaoClicado(true);
+    };
+    function setLinkDois () {
+      setLinkHref('https://sacola.pagseguro.uol.com.br/caf6562d-0152-4517-9833-39d26399185e');
+      setBotaoClicado(true);
+    };
+    function setLinkTres () {
+      setLinkHref('https://sacola.pagseguro.uol.com.br/5234f51e-fe22-4b88-8c92-54f3e239186e');
+      setBotaoClicado(true);
+    };
+    function setLinkQuatro () {
+      setLinkHref('https://sacola.pagseguro.uol.com.br/6a8d426e-c6ae-4e33-9e7c-c096069a2402');
+      setBotaoClicado(true);
+    };
+
+    function handleButtonClick(index: number) {
+      setSelectedButton(index);
+    };
 
   return (
     <div className={style.valuesContainer}>
@@ -17,7 +36,7 @@ export default function Values() {
           className={`${style.value} ${
             selectedButton === 0 ? style.selectedButton : ''
           }`}
-          onClick={() => handleButtonClick(0)}
+          onClick={() => {handleButtonClick(0); setLinkUm()}}
         >
           $ 15,00
         </button>
@@ -25,7 +44,7 @@ export default function Values() {
           className={`${style.value} ${
             selectedButton === 1 ? style.selectedButton : ''
           }`}
-          onClick={() =>{ handleButtonClick(1);}}
+          onClick={() =>{ handleButtonClick(1); setLinkDois()}}
         >
           $ 25,00
         </button>
@@ -33,7 +52,7 @@ export default function Values() {
           className={`${style.value} ${
             selectedButton === 2 ? style.selectedButton : ''
           }`}
-          onClick={() => handleButtonClick(2)}
+          onClick={() =>{ handleButtonClick(2); setLinkTres();}}
         >
           $ 35,00
         </button>
@@ -41,17 +60,27 @@ export default function Values() {
           className={`${style.value} ${
             selectedButton === 3 ? style.selectedButton : ''
           }`}
-          onClick={() => handleButtonClick(3)}
+          onClick={() => {handleButtonClick(3); setLinkQuatro();}}
         >
           $ 55,00
         </button>
       </div>
-      <div className={style.containerContinuar}>
-        <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
-          <button className={style.buttonContinue}>Continuar</button>
-        </a>
-        <span>* Sua doação será processada pelo PagBank</span>
-      </div>
+      
+      {botaoClicado ? (
+        <div className={style.containerContinuar}>
+          <a className={style.link} href={linkHref} target="_blank">
+            <button className={style.buttonContinue}>Continuar</button>
+          </a>
+          <span>* Sua doação será processada pelo PagBank</span>
+        </div>
+      ) : (
+        <div className={style.containerContinuar}>
+          <button disabled className={style.buttonContinueDisabled}>Continuar</button>
+          <span>* Selecione algum valor</span>
+        </div>
+        
+        
+      )}
     </div>
   );
 }
